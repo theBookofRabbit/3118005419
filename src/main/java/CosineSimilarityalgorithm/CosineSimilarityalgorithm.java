501a28e0ecc4b281a05bc590302251bce1bebf48
Github theBookofRabbit <Github 3172864829@qq.com>
2020-09-23 00:04:13 +0800

    2020-09-23新增了基于余弦相似度和hanlp分词器的工具类和主函数（在CosineSimilarityalgorithm类中），修改pom.xml并测试了一下jar。

---
+++ src/main/java/CosineSimilarityalgorithm/CosineSimilarityalgorithm.java
@@ -0,0 +1,51 @@
+package CosineSimilarityalgorithm;
+
+import java.io.*;
+
+/**
+ * 计算两个字符串的相识度
+ */
+public class CosineSimilarityalgorithm {
+
+    //通过文件读取内容到字符串里，超过2GB的文件将不能读取
+    public static String content1 = null;
+    public static  String content2 = null;
+
+    public static void main(String[] args) {
+        System.out.println("原文文件："+args[0]+"\n\n抄袭版论文文件："+args[1]+"\n\n答案文件："+args[2]+"\n");
+        if(args[0] == null){
+            System.out.println("您没有输入原文文件路径");
+        }
+        if(args[1] == null){
+            System.out.println("您没有输入抄袭版论文的文件路径");
+        }
+        if(args[2] == null){
+            System.out.println("您没有输入答案文件路径");
+        }
+
+        try {
+            content1 = FileUtils.readFile(args[0]);
+            content2 = FileUtils.readFile(args[1]);
+        } catch (IOException e) {
+            System.out.println("读写前两份文件出错，请检查文件路径。");
+        }
+        double  score=CosineSimilarity.getSimilarity(content1,content2);
+        java.text.DecimalFormat   df   =new   java.text.DecimalFormat("#0.00");//截断保留小数点后两位和一位个位数
+        String result = df.format(score);
+        System.out.println("=================================================");
+        System.out.println("\n\n相似度："+result);
+        System.out.println("=================================================");
+
+        try{
+            File file = new File(args[2]);
+            PrintStream ps = new PrintStream(new FileOutputStream(file));
+            ps.append("相似度："+result);//文本追加写入，如果不存在文件会创建，
+        }
+        catch (FileNotFoundException e){
+            System.out.println("没有在您的电脑上找到该文件路径，无法读写");
+        }
+
+
+    }
+
+}
